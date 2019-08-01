89bad8c6fc5d541b2abc77608b49d5b2334f5190
Edson Cândido <edson.c.a.j@hotmail.com>
2019-08-01 00:42:41 -0300

    Cliado a Classe Mercado Bitcoin

--- src/model/MercadoBitcoin.java
+++ src/model/MercadoBitcoin.java
@@ -9,8 +9,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import org.json.JSONObject;
 
@@ -21,35 +19,43 @@ import org.json.JSONObject;
 public class MercadoBitcoin {
 
     private final String url = "https://www.mercadobitcoin.net/api/";
+    private JSONObject json;
+    private String moeda = "btc";
 
-    public JSONObject getValues(String moeda) throws IOException {
-        String webservice = this.url + moeda.toUpperCase() + "/ticker";
+    public MercadoBitcoin() throws IOException {
+        getValues();
+    }
 
-        URL web = new URL(webservice);
+    public JSONObject getValues() throws IOException {
+
+        StringBuilder webservice = new StringBuilder();
+
+        webservice.append(this.url);
+        webservice.append(this.moeda.toUpperCase());
+        webservice.append("/ticker");
+
+        URL web = new URL(webservice.toString());
         HttpURLConnection conn = (HttpURLConnection) web.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("ACCEPT", "application/jason");
 
-        System.out.println(webservice);
         if (conn.getResponseCode() != 200) {
-            return null;
+            System.out.println("Erro " + conn.getResponseCode() + " ao obter dados da URL " + url);
         }
 
         BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-        String saida = br.readLine();
-        JSONObject json = new JSONObject(saida);
-        JSONObject ticker = json.getJSONObject("ticker");
 
-        return ticker;
+        JSONObject ticker = new JSONObject(br.readLine());
+        this.json = ticker.getJSONObject("ticker");
+
+        return this.json;
     }
 
     public Double getSell() throws IOException {
-
-        return getValues("btc").getDouble("sell");
+        return this.json.getDouble("sell");
     }
 
     public Double getBuy() throws IOException {
-
-        return getValues("btc").getDouble("buy");
+        return this.json.getDouble("buy");
     }
 }
