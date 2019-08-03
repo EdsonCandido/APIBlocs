df4f9bfb8145fa51467e00347e74894341923200
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2019-08-03 08:21:10 -0300

    APIBlocs: fixando erro de importação

--- src/apiblocs/APIBlocs.java
+++ src/apiblocs/APIBlocs.java
@@ -3,10 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package apiblocs;
 
 import java.io.IOException;
-import model.MercadoBitcoin;
+import model.*;
 
 /**
  *
