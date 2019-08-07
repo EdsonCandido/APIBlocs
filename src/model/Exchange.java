77cdf00235c9daeecbdb72f89cbed97e61f53d88
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2019-08-07 11:06:11 -0300

    Corrigindo CertificateException no OpenJDK 8

--- src/model/Exchange.java
+++ src/model/Exchange.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model;
 
 import java.io.BufferedReader;
@@ -11,6 +10,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
 import org.json.JSONObject;
 
 /**
@@ -18,17 +20,20 @@ import org.json.JSONObject;
  * @author Gabriel
  */
 public abstract class Exchange {
-    
+
     protected String url;
     protected JSONObject jsonTicker;
-    
-    public void getValues() throws IOException{
-       
+
+    public void getValues() throws IOException {
+
         URL web = new URL(url);
         HttpURLConnection conn = (HttpURLConnection) web.openConnection();
         conn.setRequestMethod("GET");
-        conn.setRequestProperty("ACCEPT", "application/jason");
+        conn.setRequestProperty("ACCEPT", "application/json");
         conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+
+        //Corrige exception "CertificateException" causado em algumas versões de JDK
+        HttpsURLConnection.setDefaultHostnameVerifier((String hostname, SSLSession session) -> true);
 
         if (conn.getResponseCode() != 200) {
             System.err.println("Erro " + conn.getResponseCode() + " ao obter dados da URL " + url);
@@ -37,35 +42,35 @@ public abstract class Exchange {
         BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
 
         this.jsonTicker = new JSONObject(br.readLine());
-        
+
     }
-    
-    public long getDate(){
+
+    public long getDate() {
         return this.jsonTicker.getLong("date");
     }
-    
-    public float getSell(){
+
+    public float getSell() {
         return this.jsonTicker.getFloat("sell");
     }
 
-    public float getBuy(){
+    public float getBuy() {
         return this.jsonTicker.getFloat("buy");
     }
-       
-    public float getLast(){
+
+    public float getLast() {
         return this.jsonTicker.getFloat("last");
     }
 
-    public float getHigh(){
+    public float getHigh() {
         return this.jsonTicker.getFloat("high");
     }
-    
-    public float getLow(){
+
+    public float getLow() {
         return this.jsonTicker.getFloat("low");
     }
 
-    public float getVol(){
+    public float getVol() {
         return this.jsonTicker.getFloat("vol");
     }
-    
+
 }
