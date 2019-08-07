83f487ec75683997216745154159975036161ba4
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2019-08-07 16:21:36 -0300

    Criando biblioteca ExchangeClient.jar

--- src/test/APIBlocs.java
+++ src/test/APIBlocs.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package apiblocs;
+package test;
 
 import java.io.IOException;
 import model.*;
